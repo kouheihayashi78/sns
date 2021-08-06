54793e1de072633df57be241bf1a5acca91c6648
kouheihayashi78 <kouhei.basketball7.8@gmail.com>
2021-08-07 02:47:52 +0900

    first commit

---
+++ app/Http/Controllers/Auth/TimelineController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use \App\Models\Tweet;
+
+class TimelineController extends Controller
+{
+    public function showTimelinePage()
+    {
+        $tweets = Tweet::latest()->get();  // <--- 追加
+        return view('auth.timeline', compact('tweets'));   // <--- 変更
+    }
+
+    public function postTweet(Request $request)
+    {
+        $validator = $request->validate([
+            'tweet' => ['required', 'string', 'max:280'],
+        ]);
+        Tweet::create([
+            'user_id' => Auth::user()->id,
+            'tweet' => $request->tweet,
+            'created_at' => $request->created_at
+        ]);
+        return back();
+    }
+}
